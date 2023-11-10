97ca98381ac20443bbbb074a09ac0d0f884c7e88
rokaf340 <rokaf340@gmail.com>
2023-11-10 23:33:16 +0900

    Feat: Add 상수목록 작성

---
+++ src/component/data.js
@@ -0,0 +1,43 @@
+export const ORDER_PRICE_MENU = Object.freeze({
+  APPETIZER: {
+    양송이수프: 6000,
+    타파스: 5500,
+    시저샐러드: 8000,
+  },
+  MAIN: {
+    티본스테이크: 55000,
+    바비큐립: 54000,
+    해산물파스타: 35000,
+    크리스마스파스타: 25000,
+  },
+  DESSERT: {
+    초코케이크: 15000,
+    아이스크림: 5000,
+  },
+  DRINK: {
+    제로콜라: 3000,
+    레드와인: 60000,
+    샴페인: 25000,
+  },
+});
+
+export const DECEMBER_DATES = Object.freeze({
+  MIN_DATE: 1,
+  XMAS_DATE: 25,
+  MAX_DATE: 31,
+});
+
+export const GIFT_MENU = Object.freeze({
+  샴페인: 12000,
+});
+
+export const EVENT_BADGE = Object.freeze({
+  별: 5000,
+  트리: 10000,
+  산타: 20000,
+});
+
+export const PRECAUTIONS = Object.freeze({
+  MIN_PRICE: 10000,
+  MAX_ORDER: 20,
+});
