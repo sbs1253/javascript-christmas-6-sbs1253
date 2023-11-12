8b695e0804c666086ed29366dceec3090f6a1bf0
rokaf340 <rokaf340@gmail.com>
2023-11-12 23:17:34 +0900

    Feat: Add 특별할인 구현 - 달력에 별이있는 매주 일요일 + 크리스마스 당일 1,000원 추가 할인

--- src/component/discount/DayWeekDiscount.js
+++ src/component/discount/DayWeekDiscount.js
@@ -1,10 +1,30 @@
 import { DECEMBER_DATES } from '../data.js';
 
-const DayWeekDiscount = {
-  dayDivide(date) {
-    const startDate = new Date(2023, 12, date);
+class DayWeekDiscount {
+  // 월과 일은 0부터 시작함으로 -1을 해주는게 가독성에 좋음
+  constructor(date) {
+    this.startDate = new Date(
+      DECEMBER_DATES.YEAR,
+      DECEMBER_DATES.MONTH - 1,
+      date,
+    );
 
-    return DECEMBER_DATES.DAY[startDate.getDay()];
-  },
-};
-DayWeekDiscount.dayDivide(12);
+    this.Divide = this.dayDivide(this.startDate);
+    this.Discount = this.specialDiscount(this.startDate);
+  }
+
+  dayDivide(date) {
+    return DECEMBER_DATES.DAY[date.getDay()];
+  }
+
+  specialDiscount(date) {
+    const day = date.getDay();
+    let specialDiscount = 0;
+    if (day === 0 || date.getDate() === 25) specialDiscount = 1000;
+    return specialDiscount;
+  }
+
+  // 평일과 주말은 구분했지만 할인률 적용시키는것은 메뉴 함수 구현후
+}
+const discounts = new DayWeekDiscount(25);
+console.log(discounts.Discount);
